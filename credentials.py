66bdab8c2fa2b0a2c419e3b014c791c986444405
Monica Babaian <monica.babaian@gmail.com>
2018-02-16 06:19:00 -0600

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-consumer_key = 'FcfOwyaxY2a7LAwNRL8erPB61'
-consumer_secret = 'bJAFKaocVP6ixSfWsrks5oixOpIB9DrvcMEtKaLCpabbXBsiBT'
-access_token = '850924990493011968-yrwFGwr8jTquAGudFFxTpcRx4m6mQEZ'
-access_token_secret = 'jsKdb3lMFvMjYmgrW6vTcI1w3UqzCelcaYwiCd9RYKdPB'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
